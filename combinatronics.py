29d06d0423f190b7fa6231e0a078a947bfcad5e0
Rodrigo Mendoza <A01720627@itesm.mx>
2022-09-04 23:44:34 -0500

    intento

--- combinatronics.py
+++ combinatronics.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 from comparable import C
 
 
@@ -7,7 +8,7 @@ def power_set(s: list[C]) -> list[list[C]]:
         return r + [t + [s[-1]] for t in r]
     else:
         return [[]]
-
+    
 
 def sorted_nicely(s: list[list[C]]) -> list[list[C]]:
 
@@ -20,7 +21,7 @@ def sorted_nicely(s: list[list[C]]) -> list[list[C]]:
 def combinations(s: list[C], n: int) -> list[list[C]]:
     return [t for t in power_set(s) if len(t) == n]
 
-
+    
 def insert(x: C, lst: list[C], i: int) -> list[C]:
     return lst[:i] + [x] + lst[i:]
 
@@ -41,6 +42,40 @@ def permutations(s: list[C], n: int) -> list[list[C]]:
     return sum([permute(t) for t in combinations(s, n)], [])
 
 
+
+def combinations_with_repetition(s: list[C], n: int) -> list[list[C]]:
+    ret = []
+    for k in range(len(s)):
+        temp = []
+        for i in range(n):
+            temp.append(s[k])
+        
+                
+        for i in range(n):
+            for u in range(len(s)):
+                # print("vuelta")
+                # print(f'i {s[u]} u {i}')
+                # print(f'temp antes: {temp}')
+                temp[i] = s[u]
+                temp2 = temp.copy()
+                # print(temp)
+                # print(f'temp cambio: {temp2}\n')
+                temp2.sort()
+                # print(temp2)
+                ret.append(temp2)
+                # print(ret)
+                
+    # pprint(sorted(ret))
+    # print('\n')
+    return [list(item) for item in set(tuple(row) for row in ret)]
+    
+def permutations_with_repetition(s: list[C], n: int) -> list[list[C]]:
+    # print(combinations_with_repetitions(s, n))
+    # print(permute([0,0,0,1]))
+    return [list(item) for item in set(tuple(row) for row in sum([permute(t) for t in combinations_with_repetition(s, n)], []))]
+
+
+
 if __name__ == '__main__':
     from pprint import pprint
 
@@ -50,4 +85,7 @@ if __name__ == '__main__':
     # print()
     # pprint(sorted_nicely(combinations([True, False], 1)))
     # print(insert_many(5, [1, 2, 3, 4]))
-    pprint(sorted_nicely(permutations([1, 2, 3, 4], 5)))
+    # pprint(sorted_nicely(permutations([1, 2, 3, 4], 4)))
+    # pprint(sorted(combinations_with_repetitions(['a', 'b', 'c', 'd'], 2)))
+    pprint(sorted(
+                combinations_with_repetition([1, 2, 3], 4)))
